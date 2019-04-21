580dc763977686d73957d7796400168b4400314d
Rajashree Naik <rajashreenaik14@gmail.com>
2019-04-20 22:07:20 -0400

    Adding SPs and changes in views

---
+++ TakeDownViews.sql
@@ -0,0 +1,13 @@
+-- DROP VIEWS ONE BY ONE
+USE SupplyChainDB;
+DROP VIEW active_supplier;
+DROP VIEW approval_with_employee;
+DROP VIEW approval_with_supplier;
+DROP VIEW customer_program_employee;
+DROP VIEW customer_program_invoice;
+DROP VIEW customer_program_part;
+DROP VIEW evaluation_stats_employee;
+DROP VIEW evaluation_stats_supplier;
+DROP VIEW inventory_flow_status;
+DROP VIEW part_approval_supplier;
+DROP VIEW project_by_customer;
