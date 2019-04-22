0e9e8e715f4f347e35ca8ca44f9b1afbd6aef6c5
Rajashree Naik <rajashreenaik14@gmail.com>
2019-04-21 20:20:42 -0400

    Changes in SP and Functions

---
+++ Function.sql
@@ -0,0 +1,19 @@
+-- ----------------------------------------------------------------------------------------------------------------
+-- FUNCTION TO GENERATE ADDRESS OF THE CUSTOMER
+ALTER TABLE CUSTOMER
+ADD COLUMN
+CUST_ADDRESS VARCHAR(500) GENERATED ALWAYS AS (CONCAT(CUST_STREET,',', CUST_CITY,',',CUST_STATE,',', CUST_ZIP));
+ 
+-- ----------------------------------------------------------------------------------------------------------------
+-- FUNCTION TO GENERATE ADDRESS OF THE EMPLOYEE
+ALTER TABLE EMPLOYEE
+ADD COLUMN
+EMP_ADDRESS VARCHAR(500) GENERATED ALWAYS AS (CONCAT(EMP_STREET,',', EMP_CITY,',',EMP_STATE,',', EMP_ZIP));
+
+-- ----------------------------------------------------------------------------------------------------------------
+-- FUNCTION TO GENERATE ADDRESS OF THE SUPPLIER
+ALTER TABLE SUPPLIER
+ADD COLUMN
+SUPP_ADDRESS VARCHAR(500) GENERATED ALWAYS AS (CONCAT(SUPP_STREET,',', SUPP_CITY,',',SUPP_STATE,',', SUPP_ZIP));
+   
+-- ----------------------------------------------------------------------------------------------------------------
